fe2b3511834936a87d5064f469aea9601d7eaaf0
AbdullahYT12 <47686205+AbdullahYT12@users.noreply.github.com>
2020-11-02 17:59:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzY5NDcyOTYzOTcwOTkwMDkw.X5Phig.p9Qv8jzsIPYP99OpRux4JPgFEgg", // توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "!", //برفكس البوت الأساسي
+  dev : "398513421907132416" //أيديك
+};
